e452546c68fd07d593d676e9576816fa8199c4aa
Alicia <2102573@sit.singaporetech.edu.sg>
2023-10-01 23:24:59 +0800

    fixed css

--- frontend/src/features/viewOrganizations/ViewOrganizations.js
+++ frontend/src/features/viewOrganizations/ViewOrganizations.js
@@ -7,18 +7,20 @@ function ViewOrganizations() {
       <h1>Organizations</h1>
       <div className="organizations-page-container">
         <div className="filters-container">
-          <p className="filters-header">Search by category</p>
-          <p>Cat1</p>
-          <p>Cat2</p>
+          <h2 className="filters-header">Search by category</h2>
+          <div className="filters-list">
+            {/* Categories <p> might change to making a new react component with checkbox */}
+            <p>Cat1</p>
+            <p>Cat2</p>
+          </div>
         </div>
         <div className="organizations-container">
-          <p className="organizations-header">Organisations</p>
-          <div className="organizations-results">
+          <h2 className="organizations-header">Organisations</h2>
+          <div className="organizations-list">
             <OrganizationCard name="Hopeful Hearts Foundation" description="At Hopeful Hearts Foundation, our mission is to bring positive change to the lives of those in need and create a brighter future for our communities. We believe in the power of compassion, generosity, and unity to make the world a better place." />
             <OrganizationCard name="Hopeful Hearts Foundation" description="At Hopeful Hearts Foundation, our mission is to bring positive change to the lives of those in need and create a brighter future for our communities. We believe in the power of compassion, generosity, and unity to make the world a better place." />
             <OrganizationCard name="Hopeful Hearts Foundation" description="At Hopeful Hearts Foundation, our mission is to bring positive change to the lives of those in need and create a brighter future for our communities. We believe in the power of compassion, generosity, and unity to make the world a better place." />
             <OrganizationCard name="Hopeful Hearts Foundation" description="At Hopeful Hearts Foundation, our mission is to bring positive change to the lives of those in need and create a brighter future for our communities. We believe in the power of compassion, generosity, and unity to make the world a better place." />
-
           </div>
         </div>
       </div>
