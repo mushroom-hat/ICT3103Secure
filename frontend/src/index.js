291b5bdda153d414a04127a8df38f1f9784bd119
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-10-13 01:32:48 +0800

    removed bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom/client'; 
 import './index.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import App from './App'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
  
