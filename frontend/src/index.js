8b1b5abd296e782e2fd8cec797a4ec738795cbac
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-10-15 02:24:36 +0800

    updated jenkinsfile to reflect node env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { config } from './constants/backend-api'
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
+console.log(config.url.API_URL);
 root.render( 
   <React.StrictMode>
     <Provider store={store}>
