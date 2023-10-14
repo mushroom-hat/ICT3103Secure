f71d7209ff9b84a10e742543793b093130a26330
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-10-15 04:21:02 +0800

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,11 +1,10 @@
 const developmentOrigin = 'http://localhost:3000';
-const productionOriginHTTPS = 'https://charsity-frontend-container.com';
-const productionOriginHTTP = 'http://charsity-frontend-container.com';
+const productionOrigin = 'charsity-frontend-container';
 
 let allowedOrigins;
 
 if (process.env.NODE_ENV === 'production') {
-    allowedOrigins = [productionOriginHTTPS, productionOriginHTTP];
+    allowedOrigins = [productionOrigin];
     
 } else {
     allowedOrigins = [developmentOrigin, "postman"];
