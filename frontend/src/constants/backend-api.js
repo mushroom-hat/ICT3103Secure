664bca110f532b25c09ec216d42f0b45621b59d4
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-10-15 05:11:30 +0800

    update

--- frontend/src/constants/backend-api.js
+++ frontend/src/constants/backend-api.js
@@ -1,12 +1,12 @@
 const prod = {
     url: {
-      API_URL: 'http://charsity-backend-container:3500',
+      API_URL: 'https://charsity-backend-container:3500',
     }
   };
   
   const dev = {
     url: {
-      API_URL: 'http://charsity-backend-container:3500'
+      API_URL: 'http://localhost:3500'
     }
   };
   
