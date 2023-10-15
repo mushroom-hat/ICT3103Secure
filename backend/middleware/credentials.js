4e39f20914693270c1b9f394d58a33f0d68f542c
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-10-15 14:48:01 +0800

    Update credentials.js

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -4,6 +4,7 @@ const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     console.log("Origin:", origin);
     if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin); // Set the 'Access-Control-Allow-Origin' header to the request's origin if it's in allowedOrigins.
         res.header('Access-Control-Allow-Credentials', true); // set this header if origin is in allowedOrigins. This header is what CORS is looking for.
     }
     next();
