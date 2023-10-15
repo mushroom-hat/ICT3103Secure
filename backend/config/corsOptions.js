18448d85f18c8417af54882a30f8f2f90e08c005
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-10-15 14:34:59 +0800

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,4 +1,5 @@
 const allowedOrigins = require('./allowedOrigins');
+console.log("Allowed Domains: " + allowedOrigins);
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1) {
@@ -6,6 +7,7 @@ const corsOptions = {
         } else {
             callback(new Error('Not allowed by CORS'));
         }
+
     },
     credentials: true, // Corrected property name
     optionsSuccessStatus: 200, // Corrected property name
