846e950d7fa2eba8c9e1b6342f69ab2d54a3e476
Khansa Zilfa Shofia Ghazali <kzsghazali.kzsg@gmail.com>
2023-10-25 15:16:59 +0800

    fixed useAuth

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -7,17 +7,10 @@ const useAuth = () => {
     let id = '';
     let username = '';
     let roles = '';
-    let id = '';
 
     if (token) {
         const decoded = jwtDecode(token);
-<<<<<<< Updated upstream
-        const { id: decodedId, username: decodedUsername, roles: decodedRoles } = decoded.UserInfo;
-
-        id = decodedId;
-=======
         const { username: decodedUsername, roles: decodedRoles, id: decodedId } = decoded.UserInfo;
->>>>>>> Stashed changes
 
         // Assuming the user has only one role, you can modify this part for multiple roles
         if (decodedRoles) {
