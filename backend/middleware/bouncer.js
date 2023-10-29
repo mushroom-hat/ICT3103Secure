38ba29474b3d6a10378756737079f375da512ee3
Ng Zheng Hao <45364320+ngzhenghaonzh@users.noreply.github.com>
2023-10-29 19:08:31 +0800

    Update bouncer.js

--- backend/middleware/bouncer.js
+++ backend/middleware/bouncer.js
@@ -1,26 +1,22 @@
 const { check, validationResult } = require('express-validator');
 
-// Paswword validation
+// Password validation
 const validatePassword = () => {
   return check('pwd', 'Password must be at least 7 characters long').isLength({ min: 7 });
-};
-
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
-  const extractedErrors = errors.array().map((err) => ({ [err.param]: err.msg }));
-  return res.status(422).json({
-    errors: extractedErrors,
-  });
 };
 
 const bouncer = (req, res, next) => {
   console.log("the validation begins");
   const validationRule = validatePassword();
   validationRule(req, res, () => {
-    validate(req, res, next); // Execute the validation function
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const extractedErrors = errors.array().map((err) => ({ [err.param]: err.msg }));
+      return res.status(422).json({
+        errors: extractedErrors,
+      });
+    }
+    next();
   });
 };
 
