2f3b0a631876a233309ed80312cf7b0efec5ef52
mushroom <88775203+mushroom-hat@users.noreply.github.com>
2023-11-01 22:29:19 +0800

    changed logrotation to daily

--- backend/helper/dailyUploadLogs.js
+++ backend/helper/dailyUploadLogs.js
@@ -2,14 +2,14 @@ const schedule = require('node-schedule');
 const uploadLogs = require('./s3Upload');
 
 // Define the time of day you want the function to run (e.g., at midnight)
-const hourlySchedule = '0 * * * *'; // Runs every hour on the hour
+const dailySchedule = '0 0 * * *'; // Runs at 00:00 every day
 
 // Parameter to inject into the uploadFile function
 const errLog = 'errLog.log';
 const reqLog = 'reqLog.log';
 
 // Create a scheduled job
-const job = schedule.scheduleJob(hourlySchedule, function () {
+const job = schedule.scheduleJob(dailySchedule, function () {
     // Call the uploadFile function and inject the parameter
     uploadLogs(errLog);
     uploadLogs(reqLog);
