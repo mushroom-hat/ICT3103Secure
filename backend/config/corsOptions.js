7e7ca3dadc30c4ac7727e20793708166f16e8b55
Don Don Donki <jia.zheeeee@gmail.com>
2023-11-03 04:02:45 +0800

    Update Cors

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -5,8 +5,7 @@ const corsOptions = {
         if (allowedOrigins.includes('*') || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
-            callback(null, true);
-            //callback(new Error('Not allowed by CORS'));
+            callback(new Error('Not allowed by CORS'));
         }
 
     },
