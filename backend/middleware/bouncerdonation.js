51c7cf09afb07c1d6515fde6c5fb79fec48d0157
NgZhengHaoN <90131193+NgZhengHaoN@users.noreply.github.com>
2023-11-04 17:10:52 +0800

    Donation valderminer putin over the wall

---
+++ backend/middleware/bouncerdonation.js
@@ -0,0 +1,54 @@
+const { check, validationResult } = require('express-validator');
+
+
+const validateDonation = () => {
+    return check('amount', 'Please check your donation value again.')
+      .custom((value) => {
+        const re = /^\d+(\.\d+)?$/; // match numbers with any number of decimal places
+        if (re.test(value)) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+  };
+  
+
+const bouncerdonation = async (req, res, next) => {
+  console.log("the validation begins");
+
+  const donationValidation = validateDonation();
+
+
+  let donationValid = false;
+  let donationErrors;
+
+  // Donation validation
+  const donationReq = {...req};
+  await new Promise((resolve) => {
+    donationValidation(donationReq, res, () => {
+      donationErrors = validationResult(donationReq);
+      if (!donationErrors.isEmpty()) {
+        console.log("Donation validation errors:", donationErrors.array());
+      } else {
+        donationValid = true;
+      }
+      resolve(); // Resolve the promise
+    });
+  });
+
+  if (!donationValid) {
+    // Handle the case where validations fail
+    return res.status(422).json({
+        errors: donationErrors.array(),
+    });
+
+  }
+
+  // If valid, continue with the next middleware
+  next();
+
+}
+
+module.exports = bouncerdonation;
+  
--- backend/routes/donationsRoutes.js
+++ backend/routes/donationsRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const donationsController = require('../controllers/donationsController')
 const verifyJWT = require('../middleware/verifyJWT')
 const verifyRole = require('../middleware/verifyRole')
+const bouncerdonation = require('../middleware/bouncerdonation.js');
 
 router.route('/')
     .get(donationsController.getAllDonations) // READ
-    .post(verifyJWT,verifyRole(['Donator']),donationsController.createNewDonation) // CREATE
+    .post(verifyJWT,verifyRole(['Donator']), bouncerdonation, donationsController.createNewDonation) // CREATE
     .patch(donationsController.deleteDonation) // DELETE
 
 module.exports = router
