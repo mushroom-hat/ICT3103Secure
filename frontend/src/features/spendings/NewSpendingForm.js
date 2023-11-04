db648fe20dfc6a1b47ecdd60a911e132a7061e40
Alicia <2102573@sit.singaporetech.edu.sg>
2023-11-04 22:53:40 +0800

    Update NewSpendingForm.js

--- frontend/src/features/spendings/NewSpendingForm.js
+++ frontend/src/features/spendings/NewSpendingForm.js
@@ -1,30 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { CheckCircleFill, XCircleFill } from 'react-bootstrap-icons';
-
+import React, { useState, useEffect } from "react";
+import { CheckCircleFill, XCircleFill } from "react-bootstrap-icons";
 import { useAddNewSpendingMutation } from "./spendingsApiSlice";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersApiSlice";
 import useAuth from "../../hooks/useAuth";
+import Particle from "../../components/Particle";
+import { Container } from "react-bootstrap";
+import NavBar from "../../components/Navbar";
 
 const AMOUNT_REGEX = /^[0-9]+(\.[0.9]{1,2})/;
 const DESCRIPTION_REGEX = /^.{0,500}/;
 
 const NewSpendingForm = () => {
-  const [addNewSpending, {
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  }] = useAddNewSpendingMutation();
+  const [addNewSpending, { isLoading, isSuccess, isError, error }] =
+    useAddNewSpendingMutation();
 
   const navigate = useNavigate();
   const { username, userId } = useAuth();
-  const [organizationId, setOrganizationId] = useState(userId || ''); // Assuming you have organizationId
-  const [amount, setAmount] = useState('');
-  const [description, setDescription] = useState(''); // Added description state
+  const [organizationId, setOrganizationId] = useState(userId || ""); // Assuming you have organizationId
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState(""); // Added description state
   const [validAmount, setValidAmount] = useState(false);
   const [validDescription, setValidDescription] = useState(false);
 
@@ -36,20 +32,19 @@ const NewSpendingForm = () => {
   useEffect(() => {
     setValidAmount(AMOUNT_REGEX.test(amount));
     setValidDescription(DESCRIPTION_REGEX.test(description));
-
   }, [amount, description]);
 
   useEffect(() => {
     if (isSuccess) {
-      setOrganizationId('');
-      setAmount('');
-      setDescription(''); // Clear description
+      setOrganizationId("");
+      setAmount("");
+      setDescription(""); // Clear description
       setShowSuccessMessage(true);
       setIsSuccessMessage(true);
       setTimeout(() => {
         setShowSuccessMessage(false);
         setIsSuccessMessage(false);
-        navigate('/dash/spending');
+        navigate("/dash/spending");
       }, 3000);
     }
   }, [isSuccess, navigate]);
@@ -57,71 +52,123 @@ const NewSpendingForm = () => {
   const onAmountChanged = (e) => setAmount(e.target.value);
   const onDescriptionChanged = (e) => setDescription(e.target.value);
 
-  const organizationUsers = users.filter(user => user.roles === "Organization");
-  const canSave = Boolean(organizationId) && validAmount && validDescription && !isLoading;
-  console.log(organizationId)
+  const organizationUsers = users.filter(
+    (user) => user.roles === "Organization"
+  );
+  const canSave =
+    Boolean(organizationId) && validAmount && validDescription && !isLoading;
+  console.log(organizationId);
 
   const onSaveSpendingClicked = async (e) => {
     e.preventDefault();
     if (canSave) {
-      await addNewSpending({ organization: organizationId, amount, description });
+      await addNewSpending({
+        organization: organizationId,
+        amount,
+        description,
+      });
     }
   };
 
-  const validAmountClass = !validAmount ? 'form__input--incomplete' : '';
-  const validDescriptionClass = !validDescription ? 'form__input--incomplete' : '';
+  const validAmountClass = !validAmount ? "form__input--incomplete" : "";
+  const validDescriptionClass = !validDescription
+    ? "form__input--incomplete"
+    : "";
   const errClass = isError ? "errmsg" : "offscreen";
 
   return (
-    <>
-      {showSuccessMessage && (
-        <div className={isSuccessMessage ? "success-message" : "error-message"}>
-          {isSuccessMessage ? (
-            <CheckCircleFill color="green" className="mr-2" style={{ marginRight: "10px" }} />
-          ) : (
-            <XCircleFill color="red" className="mr-2" style={{ marginRight: "10px" }} />
-          )}
-          {isSuccessMessage ? "Spending added successfully!" : "Failed to add spending"}
-        </div>
-      )}
+    <Container fluid className="project-section">
+      <Particle />
+      <NavBar />
+      <Container>
+        {showSuccessMessage && (
+          <div
+            className={isSuccessMessage ? "success-message" : "error-message"}
+          >
+            {isSuccessMessage ? (
+              <CheckCircleFill
+                color="green"
+                className="mr-2"
+                style={{ marginRight: "10px" }}
+              />
+            ) : (
+              <XCircleFill
+                color="red"
+                className="mr-2"
+                style={{ marginRight: "10px" }}
+              />
+            )}
+            {isSuccessMessage
+              ? "Spending added successfully!"
+              : "Failed to add spending"}
+          </div>
+        )}
 
-      <p className={errClass}>{error?.data?.message}</p>
+        <p className={errClass}>{error?.data?.message}</p>
+        <h1 className="project-heading">New Spending</h1>
+        <form
+          className="form"
+          onSubmit={onSaveSpendingClicked}
+          style={{
+            display: "grid",
+            gridTemplateColumns: "auto 1fr",
+            gridGap: "0.5rem",
+            zIndex: 1,
+            position: "relative",
+            alignItems: "center"
+          }}
+        >
+          <label
+            className="form__label"
+            htmlFor="organization"
+            style={{ color: "white" }}
+          >
+            Organization:
+          </label>
+          <p style={{ color: "white", margin: 0 }}>{username}</p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              color: "white",
+            }}
+          >
+            <label className="form__label" htmlFor="amount">
+              Amount:
+            </label>
+            <p style={{ margin: 0 }}>$</p>
+          </div>
 
-      <form className="form" onSubmit={onSaveSpendingClicked}>
-        <div className="form__title-row">
-          <h2>New Spending</h2>
-        </div>
-        <label className="form__label" htmlFor="organization">
-          Organization:
-        </label>
-        <p>{username}</p>
-        <label className="form__label" htmlFor="amount">
-          Amount:
-        </label>
-        <input
-          className={`form__input ${validAmountClass}`}
-          id="amount"
-          name="amount"
-          type="text"
-          value={amount}
-          onChange={onAmountChanged}
-        />
-        <label className="form__label" htmlFor="description">
-          Description:
-        </label>
-        <input
-          className={`form__input ${validDescriptionClass}`}
-          id="description"
-          name="description"
-          type="text"
-          value={description}
-          onChange={onDescriptionChanged}
-        />
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
-    </>
+          <input
+            className={`form__input ${validAmountClass}`}
+            id="amount"
+            name="amount"
+            type="text"
+            value={amount}
+            onChange={onAmountChanged}
+          />
+          <label
+            className="form__label"
+            htmlFor="description"
+            style={{ color: "white" }}
+          >
+            Description:
+          </label>
+          <input
+            className={`form__input ${validDescriptionClass}`}
+            id="description"
+            name="description"
+            type="text"
+            value={description}
+            onChange={onDescriptionChanged}
+          />
+          <button type="submit" className="rounded-button">
+            Submit
+          </button>
+        </form>
+      </Container>
+    </Container>
   );
 };
 
