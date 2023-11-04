5e43b03dea17add1b5113183c97454bca3ece9ea
Alicia <2102573@sit.singaporetech.edu.sg>
2023-11-04 20:56:04 +0800

    Update UI

--- frontend/src/features/spendings/NewSpendingForm.js
+++ frontend/src/features/spendings/NewSpendingForm.js
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useAddNewSpendingMutation } from "./spendingsApiSlice";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersApiSlice";
+import Particle from "../../components/Particle";
+import { Container } from "react-bootstrap";
+import NavBar from "../../components/Navbar";
 
 const AMOUNT_REGEX = /^[0-9]+(\.[0-9]{1,2})?/;
 
 const NewSpendingForm = () => {
-  const [addNewSpending, {
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  }] = useAddNewSpendingMutation();
+  const [addNewSpending, { isLoading, isSuccess, isError, error }] =
+    useAddNewSpendingMutation();
 
   const navigate = useNavigate();
 
-  const [organizationId, setOrganizationId] = useState('');
-  const [amount, setAmount] = useState('');
+  const [organizationId, setOrganizationId] = useState("");
+  const [amount, setAmount] = useState("");
   const [validAmount, setValidAmount] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
@@ -31,12 +28,12 @@ const NewSpendingForm = () => {
 
   useEffect(() => {
     if (isSuccess) {
-      setOrganizationId('');
-      setAmount('');
+      setOrganizationId("");
+      setAmount("");
       setShowSuccessMessage(true);
       setTimeout(() => {
         setShowSuccessMessage(false);
-        navigate('/dash/spending');
+        navigate("/dash/spending");
       }, 3000);
     }
   }, [isSuccess, navigate]);
@@ -44,7 +41,9 @@ const NewSpendingForm = () => {
   const onOrganizationChanged = (e) => setOrganizationId(e.target.value);
   const onAmountChanged = (e) => setAmount(e.target.value);
 
-  const organizationUsers = users.filter(user => user.roles === "Organization");
+  const organizationUsers = users.filter(
+    (user) => user.roles === "Organization"
+  );
 
   const canSave = Boolean(organizationId) && validAmount && !isLoading;
 
@@ -55,53 +54,59 @@ const NewSpendingForm = () => {
     }
   };
 
-  const validAmountClass = !validAmount ? 'form__input--incomplete' : '';
+  const validAmountClass = !validAmount ? "form__input--incomplete" : "";
   const errClass = isError ? "errmsg" : "offscreen";
 
   return (
-    <>
-      {showSuccessMessage && (
-        <div className="success-message">
-          Spending added successfully!
-        </div>
-      )}
+    <Container fluid className="project-section">
+      <Particle />
+      <NavBar />
+      <Container>
+        {showSuccessMessage && (
+          <div className="success-message">Spending added successfully!</div>
+        )}
 
-      <p className={errClass}>{error?.data?.message}</p>
-
-      <form className="form" onSubmit={onSaveSpendingClicked}>
-        <div className="form__title-row">
-          <h2>New Spending</h2>
-        </div>
-        <label className="form__label" htmlFor="organization">
-          Organization:
-        </label>
-        <select
-          id="organization"
-          name="organization"
-          className={`form__select`}
-          value={organizationId}
-          onChange={onOrganizationChanged}
-        >
-          <option value="">Select an organization</option>
-          {organizationUsers.map(user => (
-            <option key={user._id} value={user._id}>
-              {user.username}
-            </option>
-          ))}
-        </select>
-        <label className="form__label" htmlFor="amount">
-          Amount:
-        </label>
-        <input
-          className={`form__input ${validAmountClass}`}
-          id="amount"
-          name="amount"
-          type="text"
-          value={amount}
-          onChange={onAmountChanged}
-        />
-      </form>
-    </>
+        <p className={errClass}>{error?.data?.message}</p>
+        <h1 className="project-heading">New Spending</h1>
+        <form className="form" onSubmit={onSaveSpendingClicked} style={{
+            display: "grid",
+            gridTemplateColumns: "auto 1fr",
+            gridGap: "0.5rem",
+            zIndex: 1,
+            position: "relative",
+            paddingTop: "0.5rem"
+          }}>
+          <label className="form__label" htmlFor="organization" style={{ color: "white" }}>
+            Organization:
+          </label>
+          <select
+            id="organization"
+            name="organization"
+            className={`form__select`}
+            value={organizationId}
+            onChange={onOrganizationChanged}
+          >
+            <option value="">Select an organization</option>
+            {organizationUsers.map((user) => (
+              <option key={user._id} value={user._id}>
+                {user.username}
+              </option>
+            ))}
+          </select>
+          <label className="form__label" htmlFor="amount" style={{ color: "white" }}>
+            Amount:
+          </label>
+          <input
+            className={`form__input ${validAmountClass}`}
+            id="amount"
+            name="amount"
+            type="text"
+            value={amount}
+            onChange={onAmountChanged}
+          />
+        </form>
+      </Container>
+    </Container>
   );
 };
 
--- frontend/src/features/spendings/SpendingsList.js
+++ frontend/src/features/spendings/SpendingsList.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { useGetSpendingsQuery } from './spendingsApiSlice';
-import Spending from './Spending';
-
+import React from "react";
+import { useGetSpendingsQuery } from "./spendingsApiSlice";
+import Spending from "./Spending";
+import Particle from "../../components/Particle";
+import { Container } from "react-bootstrap";
+import NavBar from "../../components/Navbar";
 
 const SpendingsList = () => {
   const {
@@ -14,41 +16,43 @@ const SpendingsList = () => {
     refetchOnFocus: true,
     refetchOnMountOrArgChange: true,
   });
-  console.log("myspendings", spendings)
-  console.log("isLoading: ", isLoading)
-  console.log("isSuccess: ", isSuccess)
-  console.log("isError: ", isError)
-  console.log("Hello: ")
-
-
+  console.log("myspendings", spendings);
+  console.log("isLoading: ", isLoading);
+  console.log("isSuccess: ", isSuccess);
+  console.log("isError: ", isError);
+  console.log("Hello: ");
 
   let content;
 
-
   if (isLoading) content = <p>Loading...</p>;
-
 
   if (isError) {
     content = <p>Not Currently Available</p>;
   }
 
-
   if (isSuccess) {
     const { ids } = spendings;
 
-
-    const tableContent = ids?.length ? (
-      ids.map((spendingId) => <Spending key={spendingId} spendingId={spendingId} />)
-    ) : null;
+    const tableContent = ids?.length
+      ? ids.map((spendingId) => (
+          <Spending key={spendingId} spendingId={spendingId} />
+        ))
+      : null;
     //console.log("mySpending:" , spendings)
 
     content = (
       <table className="table table--spendings">
         <thead className="table__thead">
           <tr>
-            <th scope="col" className="table__th spending__organization">Organization</th>
-            <th scope="col" className="table__th spending__amount">Amount</th>
-            <th scope="col" className="table__th spending__edit">Description</th>
+            <th scope="col" className="table__th spending__organization">
+              Organization
+            </th>
+            <th scope="col" className="table__th spending__amount">
+              Amount
+            </th>
+            <th scope="col" className="table__th spending__edit">
+              Description
+            </th>
           </tr>
         </thead>
         <tbody>{tableContent}</tbody>
@@ -56,9 +60,16 @@ const SpendingsList = () => {
     );
   }
 
-
-  return content;
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <NavBar />
+      <Container>
+        <h1 className="project-heading">View Your Spendings</h1>
+        {content}
+      </Container>
+    </Container>
+  );
 };
-
 
 export default SpendingsList;
